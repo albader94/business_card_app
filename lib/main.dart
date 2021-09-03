37775e9c5acf344b6e12e34ca89034b8376b949d
Bader Albader <b.albader94@gmail.com>
2021-09-04 02:52:13 +0300

    icons and containers
    Played around with icons and containers and their properties.
    
    Also played around with rows and columns to layout

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             //mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
@@ -22,9 +24,68 @@ class MyApp extends StatelessWidget {
               Text(
                 'Bader Albader',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
                   fontSize: 40.0,
+                ),
+              ),
+              Text(
+                "DESIGNER & FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal[100],
+                  fontSize: 18.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+965 12345678',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 18,
+                        color: Colors.teal[900],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'albader@email.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 18,
+                        color: Colors.teal[900],
+                      ),
+                    )
+                  ],
                 ),
               ),
             ],
